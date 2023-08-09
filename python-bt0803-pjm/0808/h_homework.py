de4de839c2fad6dde5c11f149b30952e1ff1c86f
Jimin Park <meanof5k@naver.com>
2023-08-09 20:23:06 +0900

    230808 python

--- python-bt0803-pjm/0808/h_homework.py
+++ python-bt0803-pjm/0808/h_homework.py
@@ -24,6 +24,23 @@ class Car:
 # 배터리를 충전하는 charge() 메서드가 있습니다. 최대 충전량을 초과하도록 충적할 수 없고, 0보다 작은 값으로 충전할 수 없습니다.
 # 현재 주유량과 충전량을 모두 확인할 수 있는 hybrid_info() 메소드가 있습니다.
 
+class Hybrid(Car):
+    max_battery = 30
+
+    def __init__(self, oil, battery):
+        super().__init__(oil) # 슈퍼 클래스의 생성자를 호출하여 oil 속성을 초기화
+        self.battery = battery
+
+    def charge(self, battery):
+        if battery <= 0:
+            return # 0 이하로 충전하는 것을 방지
+        self.battery = battery
+        if self.battery > Hybrid.max_battery:
+            self.battery = Hybrid.max_battery # 현재 배터리 충전량을 최대 충전량으로 설정
+
+    def hybrid_info(self):
+        self.car_info() # 슈퍼 클래스의 car_info를 호출
+        print('현재 충전상태: {}'.format(self.battery))
 
 # 3. 다음과 같은 방식으로 전체 동작을 확인할 수 있습니다.
 car = Hybrid(0, 0)
